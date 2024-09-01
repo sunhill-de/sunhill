f1ed8d0a675d87acfc97aff0d65ec9be7e359fe8
Klaus Dimde <klaus@dimde.info>
2024-09-01 18:51:47 +0200

    Reimplemented Checks facade

---
+++ src/Facades/Checks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sunhill\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Checks extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'checks';
+    }
+}
