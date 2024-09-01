ab78b8e10657a126ccf4ac8b05f297dea40af08d
Klaus Dimde <klaus@dimde.info>
2024-09-01 18:50:00 +0200

    Basic expcetion

---
+++ src/Exceptions/SunhillException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @file SunhillException.php
+ * Provides root for all sunhill exceptions
+ */
+
+namespace Sunhill\Exceptions;
+
+
+/**
+ * Root for sunhill exceptions
+ * @author lokal
+ *
+ */
+class SunhillException extends \Exception {}
