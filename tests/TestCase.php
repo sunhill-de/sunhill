f2c169fe9d38a70efc381c2e9d62d02236683663
Klaus Dimde <klaus@dimde.info>
2024-09-27 12:18:59 +0200

    Included testbench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,12 @@ namespace Sunhill\Tests;
 
 use \Orchestra\Testbench\TestCase as Orchestra;
 use Sunhill\SunhillServiceProvider;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 
 class TestCase extends Orchestra
 {
+    use WithWorkbench;
+    
     protected function getPackageProviders($app)
     {
         return [
