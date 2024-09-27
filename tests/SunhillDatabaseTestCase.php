94c5299252da968797062a32508da1e48e96d9b2
Klaus Dimde <klaus@dimde.info>
2024-09-27 12:18:17 +0200

    Call migrations

--- tests/SunhillDatabaseTestCase.php
+++ tests/SunhillDatabaseTestCase.php
@@ -2,11 +2,32 @@
 
 namespace Sunhill\Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\Attributes\WithEnv;
+
 class SunhillDatabaseTestCase extends TestCase
 {
 
+    use RefreshDatabase;
+    /*
     protected function defineDatabaseMigrations()
     {
+        $this->artisan('migrate', ['--database'=>'testbench'])->run();
         
+        $this->beforeApplicationDestroyed(
+            fn() => $this->artisan($this, 'migrate:rollback', ['--database'=>'testbench'])
+            );
     }
+    */
+    protected function getEnvironmentSetUp($app)
+    {
+        # Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+    
 }
