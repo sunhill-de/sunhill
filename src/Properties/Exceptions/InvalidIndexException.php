3ad12e9f6e98b85e47d2eccdd670f6bb48769a59
Klaus Dimde <klaus@dimde.info>
2024-10-11 19:04:52 +0200

    Reviewed exceptions

---
+++ src/Properties/Exceptions/InvalidIndexException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file InvalidIndexException.php
+ * Provides the InvalidValidException
+ * Lang en
+ * Reviewstatus: 2024-10-11
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
+namespace Sunhill\Properties\Exceptions;
+
+/**
+ * An exception that is raised, if a invalid array index was used in a ArrayProperty
+ * @author lokal
+ */
+class InvalidIndexException extends PropertyException 
+{
+}
