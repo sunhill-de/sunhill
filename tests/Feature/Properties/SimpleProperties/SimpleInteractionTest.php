50914af1f37884cd6bb277b56844ecbd704b9280
Klaus Dimde <klaus@dimde.info>
2024-10-11 19:07:50 +0200

    More integration tests

--- tests/Feature/Properties/SimpleProperties/SimpleInteractionTest.php
+++ tests/Feature/Properties/SimpleProperties/SimpleInteractionTest.php
@@ -3,6 +3,10 @@
 use Sunhill\Tests\SunhillTestCase;
 use Sunhill\Tests\TestSupport\Storages\DummyStorage;
 use Sunhill\Types\TypeFloat;
+use Sunhill\Properties\Exceptions\UninitializedValueException;
+use Sunhill\Properties\ArrayProperty;
+use Sunhill\Properties\Exceptions\PropertyKeyDoesntExistException;
+use Sunhill\Properties\Exceptions\InvalidIndexException;
 
 uses(SunhillTestCase::class);
 
@@ -10,7 +14,52 @@ test('read initialized value', function()
 {
     $storage = new DummyStorage();
     $test = new TypeFloat();
-    $test->setName('KeyB')->setStorage($storage);
+    $test->setName('keyB')->setStorage($storage);
     
     expect($test->getValue())->toBe(3.56);
 });
+
+it('fails when reading uninitialized value',function()
+{
+    $storage = new DummyStorage();
+    $test = new TypeFloat();
+    $test->setName('unknown')->setStorage($storage);
+    
+    $dummy = $test->getValue();
+})->throws(UninitializedValueException::class);
+
+test('read uninitialized value with default', function()
+{
+    $storage = new DummyStorage();
+    $test = new TypeFloat();
+    $test->setName('unknown')->setStorage($storage)->setDefault(1.23);
+    
+    expect($test->getValue())->toBe(1.23);
+});
+
+test('read array value', function()
+{
+    $storage = new DummyStorage();
+    $test = new ArrayProperty();
+    $test->setName('keyC')->setStorage($storage);
+    
+    expect($test->offsetGet(1))->toBe(2);
+});
+
+it('fails when reading an uninitialized array value', function()
+{
+    $storage = new DummyStorage();
+    $test = new ArrayProperty();
+    $test->setName('unknown')->setStorage($storage);
+    
+    expect($test->offsetGet(1))->toBe(2);    
+})->throws(UninitializedValueException::class);
+
+it('fails when reading an uninitialized array index', function()
+{
+    $storage = new DummyStorage();
+    $test = new ArrayProperty();
+    $test->setName('keyC')->setStorage($storage);
+    
+    expect($test->offsetGet(999))->toBe(2);    
+})->throws(InvalidIndexException::class);
