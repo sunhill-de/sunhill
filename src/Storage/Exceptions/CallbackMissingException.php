425f541aabb271fc2d479f408c1137f0a2872ce4
Klaus Dimde <klaus@dimde.info>
2024-10-22 18:14:49 +0200

    Exception for callbackStorage

---
+++ src/Storage/Exceptions/CallbackMissingException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file CallbackMissingException.php
+ * Provides the CallbackMissingException
+ * Lang en
+ * Reviewstatus: 2024-10-09
+ * Localization: none
+ * Documentation: complete
+ */
+
+namespace Sunhill\Storage\Exceptions;
+
+/**
+ * This exception is thrown when an expected callback is missing
+ * 
+ * @author klaus
+ *
+ */
+class CallbackMissingException extends StorageException {}
