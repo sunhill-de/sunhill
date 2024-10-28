386445b36b6cd45fef3e19ca6e49648f981bcd36
Klaus Dimde <klaus@dimde.info>
2024-10-28 11:50:54 +0100

    Tests for element access

---
+++ src/Properties/Exceptions/PropertyNotFoundException.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file PropertyNotFoundException.php
+ * Provides the PropertyNotFoundException
+ * Lang en
+ * Reviewstatus: 2024-10-28
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
+namespace Sunhill\Properties\Exceptions;
+
+/**
+ * An exception that is raised, when a property of a record property is called that
+ * doesn't exist.
+ * @author lokal
+ */
+class PropertyNotFoundException extends PropertyException 
+{
+}
--- tests/Unit/Properties/RecordProperty/ElementTest.php
+++ tests/Unit/Properties/RecordProperty/ElementTest.php
@@ -12,6 +12,9 @@ use Sunhill\Properties\Exceptions\InvalidInclusionException;
 use Sunhill\Properties\Exceptions\NotAllowedInclusionException;
 use Sunhill\Tests\TestSupport\Properties\ChildRecordProperty;
 use Sunhill\Tests\TestSupport\Properties\ParentRecordProperty;
+use Sunhill\Storage\AbstractStorage;
+use Sunhill\Properties\Exceptions\PropertyNotFoundException;
+use Sunhill\Properties\ArrayProperty;
 
 uses(SunhillTestCase::class);
 
@@ -203,4 +206,127 @@ it('fails including a record when same element names', function()
     $container->appendElement($subrecord,'','include');
 })->throws(PropertyNameAlreadyGivenException::class);
 
+test('Reading a property', function()
+{
+    $storage = \Mockery::mock(AbstractStorage::class);
+    $storage->shouldReceive('getValue')->once()->with('test1')->andReturn(2);
+    $container = new RecordProperty();
+    $container->setStorage($storage);
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    expect($container->element1)->toBe(2);
+});
 
+it('Fails when reading unknown property', function()
+{
+    $container = new RecordProperty();
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->nonexisting;    
+})->throws(PropertyNotFoundException::class);
+
+test('Reading an array property', function()
+{
+    $storage = \Mockery::mock(AbstractStorage::class);
+    $storage->shouldReceive('getIndexedValue')->once()->with('test1',1)->andReturn(2);
+    $container = new RecordProperty();
+    $container->setStorage($storage);
+    $element1 = new ArrayProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    expect($container->element1[1])->toBe(2);
+});
+
+it('Fails when reading unknown array property', function()
+{
+    $container = new RecordProperty();
+    $element1 = new ArrayProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->nonexisting[1];
+})->throws(PropertyNotFoundException::class);
+
+test('Writing a property', function()
+{
+    $storage = \Mockery::mock(AbstractStorage::class);
+    $storage->shouldReceive('setValue')->once()->with('test1',2);
+    $container = new RecordProperty();
+    $container->setStorage($storage);
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->element1 = 2;
+});
+
+it('Fails when writing unknown property', function()
+{
+    $container = new RecordProperty();
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->nonexisting = 2;
+})->throws(PropertyNotFoundException::class);
+
+test('Writing an array property', function()
+{
+    $storage = \Mockery::mock(AbstractStorage::class);
+    $storage->shouldReceive('setIndexedValue')->once()->with('test1',1,2);
+    $container = new RecordProperty();
+    $container->setStorage($storage);
+    $element1 = new ArrayProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->element1[1] = 2;
+});
+
+it('Fails when writing unknown array property', function()
+{
+    $container = new RecordProperty();
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->nonexisting[1] = 2;
+})->throws(PropertyNotFoundException::class);
+
+test('Appending an array property', function()
+{
+    $storage = \Mockery::mock(AbstractStorage::class);
+    $storage->shouldReceive('setIndexedValue')->once()->with('test1',null,2);
+    $container = new RecordProperty();
+    $container->setStorage($storage);
+    $element1 = new ArrayProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->element1[] = 2;
+});
+
+it('Fails when appending unknown array property', function()
+{
+    $container = new RecordProperty();
+    $element1 = new NonAbstractProperty();
+    $element2 = new NonAbstractProperty();
+    $container->appendElement($element1,'test1')->getName();
+    $container->appendElement($element2,'test2')->getName();
+    
+    $container->nonexisting[] = 2;
+})->throws(PropertyNotFoundException::class);
