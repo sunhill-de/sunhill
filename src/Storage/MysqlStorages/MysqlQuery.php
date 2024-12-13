de55bab6d52292bb97ae293a7c35ceecf408f20d
Klaus Dimde <klaus@dimde.info>
2024-12-13 07:03:28 +0100

    Implemented stub version of MysqlQuery

--- src/Storage/MysqlStorages/MysqlQuery.php
+++ src/Storage/MysqlStorages/MysqlQuery.php
@@ -3,10 +3,13 @@
 namespace Sunhill\Storage\MysqlStorages;
 
 use Sunhill\Basic\Base;
+use Illuminate\Support\Facades\DB;
 
 class MysqlQuery extends Base
 {
         
+    protected $current_query;
+    
     protected $basic_table = '';
     
     protected $tables = [];
@@ -20,20 +23,49 @@ class MysqlQuery extends Base
         parent::__construct();
         $this->basic_table = $basic_table;
         $this->addTable($basic_table);
+        $this->current_query = DB::table($basic_table.' as a');
+    }
+    
+    private function createDescriptor(string $name, string $join, string $letter): \stdClass
+    {
+        $info = new \stdClass();
+        
+        $info->name = $name;
+        $info->letter = $letter;
+        $info->join = $join;
+        
+        return $info;
+    }
+    
+    private function addToTable(string $name, string $letter, \stdClass $info)
+    {
+        $this->tables[$name] = $info;
+        $this->reverse_tables[$letter] = $info;        
     }
     
     protected function addTable(string $name, string $join = 'inner')
     {
         $current_letter = $this->next_letter++;
-        $info = new \stdClass();
         
-        $info->name = $name;
-        $info->letter = $current_letter;
-        $info->join = $join;
-        
-        $this->tables[$name] = $info;
-        $this->reverse_tables[$current_letter] = $info;
-        
+        $info = $this->createDescriptor($name, $join, $current_letter);
+        $this->addToTable($name, $current_letter, $info);
+
         return $current_letter;
+    }
+ 
+    public function first()
+    {
+        return $this->current_query->first();    
+    }
+    
+    public function get()
+    {
+        return $this->current_query->get();    
+    }
+    
+    public function __call($method, $parameters)
+    {
+        $this->current_query = $this->current_query->$method(... $parameters);
+        return $this;
     }
 }
