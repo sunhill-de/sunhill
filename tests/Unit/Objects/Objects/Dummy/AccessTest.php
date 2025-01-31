8bc66b5a90e02c8eddfd853078df4cda02b12148
Klaus Dimde <klaus@dimde.info>
2025-01-31 13:21:22 +0100

    Fixed expectation

--- tests/Unit/Objects/Objects/Dummy/AccessTest.php
+++ tests/Unit/Objects/Objects/Dummy/AccessTest.php
@@ -10,8 +10,9 @@ test('read of dummy value', function()
 {
     $storage = \Mockery::mock(PersistentPoolStorage::class);
     $storage->shouldReceive('getValue')->with('dummyint')->andReturn(123);
+    $storage->shouldReceive('getIsInitialized')->with('dummyint')->andReturn(true);
     $test = new Dummy();
     $test->setStorage($storage);
     
-    
+    expect($test->dummyint)->toBe(123);
 });
