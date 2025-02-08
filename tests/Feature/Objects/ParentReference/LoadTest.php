863daac153889e7083bc969dee0206a3f38fbced
Klaus Dimde <klaus@dimde.info>
2025-02-08 17:55:14 +0100

    Fixed dummint value

--- tests/Feature/Objects/ParentReference/LoadTest.php
+++ tests/Feature/Objects/ParentReference/LoadTest.php
@@ -13,5 +13,5 @@ test('Load a ParentReference from database (both references refer something)', f
     $test->load(17);
     expect($test->parent_int)->toBe(1111);
     expect($test->parent_reference->dummyint)->toBe(123);
-    expect($test->parent_rarray[2]->dummyint)->toBe(234);
+    expect($test->parent_rarray[2]->dummyint)->toBe(456);
 });
