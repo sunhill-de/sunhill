f4eac8a04063c269592de4808ec20715475e12a5
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-24 11:23:25 +0100

    Some operator tests

--- tests/Unit/Query/CheckerWhereTest.php
+++ tests/Unit/Query/CheckerWhereTest.php
@@ -30,27 +30,47 @@ function prepareQueryObject(array $field, $operator, array $condition)
     return $query_object;
 }
 
-test('Where statement passes with field, simple relation and const', function() 
+test('Where statement passes with field, relations and const', function($relation) 
 {
-    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'int'],'=',['type'=>'const','value'=>1]);
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'int'],'=',['type'=>'const','value'=>1]));
+    expect($test->check())->toBe(true);    
+})->with([['='],['>'],['<'],['<='],['>='],['<>'],['!='],['<=>']]);
+
+test('Where statement passes with field, equal relation and another field', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'=',['type'=>'field','field'=>'b','resulttype'=>'int']));
     expect($test->check())->toBe(true);    
 });
 
-test('Where statement passes with field, simple relation and another field', function() 
+test('Where statement passes with field, relative relation and another field', function() 
 {
-    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'=',['type'=>'field','field'=>'b','resulttype'=>'int']);
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'>',['type'=>'field','field'=>'b','resulttype'=>'int']));
+    expect($test->check())->toBe(true);    
+});
+
+test('Where statement passes with function of field, simple relation and const', function() 
+{
+    $query_object = prepareQueryObject(['type'=>'function_of_field','function'=>'upper','arguments'=>makeStdClass(['type'=>'field','field'=>'a'])],'=',['type'=>'const','value'=>'ABC']);
+    $query_object->shouldReceive('getFieldType')->with('a')->andReturn('string');
+    $test = new Checker($query_object);
     expect($test->check())->toBe(true);    
 });
 
 test('Where statement fails with field, simple relation and type mismatching const', function() 
 {
-    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'int'],'=',['type'=>'const','value'=>'abc']);
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'=',['type'=>'const','value'=>'abc']));
     $test->check();    
 })->throws(InvalidStatementException);
 
 test('Where statement fails with field, invalid relation and const', function() 
 {
-    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'bool'],'>',['type'=>'const','value'=>0]);
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'bool'],'>',['type'=>'const','value'=>0]));
+    $test->check();    
+})->throws(InvalidStatementException);
+
+test('Where statement fails with field, unknown relation and const', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'&%',['type'=>'const','value'=>0]));
     $test->check();    
 })->throws(InvalidStatementException);
 
