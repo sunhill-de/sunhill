ed7eab8e5ede1e0fd58e154be20ae49a68d8156a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-24 10:35:36 +0100

    Implemented prepareQueryObject()

--- tests/Unit/Query/CheckerWhereTest.php
+++ tests/Unit/Query/CheckerWhereTest.php
@@ -7,23 +7,50 @@ use Sunhill\Query\Exceptions\InvalidStatementException;
 
 uses(SunhillTestCase::class);
 
-test('Where statement passes with field and simple relation', function() 
+function prepareQueryObject(array $field, $operator, array $condition)
 {
     $query_object = \Mockery::mock(QueryObject::class);
-    $query_object->shouldReceive('getFieldType')->with('a')->once()->andReturn('int');
     $query_object->shouldReceive('getFields')->andReturn([]);
+    $query_object->shouldReceive('getOrderStatements')->andReturn([]);
+    $query_object->shouldReceive('getGroupFields')->andReturn([]);
+    if ($field['type'] == 'field') {
+        $query_object->shouldReceive('getFieldType')->with($field['field'])->atLeastOnce()->andReturn($field['resulttype']);
+    }    
+    if ($condition['type'] == 'field') {
+        $query_object->shouldReceive('getFieldType')->with($condition['field'])->atLeastOnce()->andReturn($condition['resulttype']);
+    }    
     $query_object->shouldReceive('getWhereStatements')->andReturn([
         makeStdClass([
             'connect'=>'and',
-            'field'=>makeStdClass(['type'=>'field','field'=>'a']),            
-            'operator'=>'=',
-            'condition'=>makeStdClass(['type'=>'const','value'=>1]),
+            'field'=>makeStdClass($field),            
+            'operator'=>$operator,
+            'condition'=>makeStdClass($condition),
         ])
     ]);
-    $query_object->shouldReceive('getOrderStatements')->andReturn([]);
-    $query_object->shouldReceive('getGroupFields')->andReturn([]);
+    return $query_object;
+}
 
-    $test = new Checker($query_object);
+test('Where statement passes with field, simple relation and const', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'int'],'=',['type'=>'const','value'=>1]);
     expect($test->check())->toBe(true);    
 });
+
+test('Where statement passes with field, simple relation and another field', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','resulttype'=>'int'],'=',['type'=>'field','field'=>'b','resulttype'=>'int']);
+    expect($test->check())->toBe(true);    
+});
+
+test('Where statement fails with field, simple relation and type mismatching const', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'int'],'=',['type'=>'const','value'=>'abc']);
+    $test->check();    
+})->throws(InvalidStatementException);
+
+test('Where statement fails with field, invalid relation and const', function() 
+{
+    $test = new Checker(prepareQueryObject(['type'=>'field','field'=>'a','type'=>'bool'],'>',['type'=>'const','value'=>0]);
+    $test->check();    
+})->throws(InvalidStatementException);
 
