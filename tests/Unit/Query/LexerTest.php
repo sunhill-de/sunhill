289990e2d9e7e202463b5d9fa63af7b01f8856ed
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-25 13:01:26 +0100

    Added additonal lexems

--- tests/Unit/Query/LexerTest.php
+++ tests/Unit/Query/LexerTest.php
@@ -8,19 +8,48 @@ test('Test EOL', function()
     expect($test->getNextToken())->tBe(null);
 });
 
-test('Test special chars',function($input, $token)
+test('Test special chars',function($input, $token, $value = null)
 {
     $test = new Lexer($input);
-    expect($test->getNextToken())->toBe($token);
+    $token = $test->getNextToken();
+    expect($token->type)->toBe($token);
+    if ($value) {
+        expect($token->vaue)->toBe($value);
+    }            
 })->with([
-  'identifier'=>['abc def','abc'],  
-  'identifier with numbers and underscore'=>['abc_d3 def','abc_d3'],  
-  'identifier starting with underscore'=>['_abc def','_abc'],  
-  'integer'=>['123 def','123'],  
-  'float'=>['1.23 def','1.23'],  
+  'identifier'=>['abc def','ident','abc'],  
+  'identifier with numbers and underscore'=>['abc_d3 def','ident','abc_d3'],  
+  'identifier starting with underscore'=>['_abc def','ident','_abc'],  
+  'integer'=>['123 def','int','123'],  
+  'float'=>['1.23 def','float','1.23'],  
   '('=>['(def','('],  
-  '('=>[')def',')'],  
+  ')'=>[')def',')'],  
   '='=>['=def','='],  
+  '-'=>['-def','-'],  
+  '+'=>['+def','+'],  
+  '*'=>['*def','*'],  
+  '/'=>['/def','/'],  
+  '=='=>['==def','=='],  
+  '<='=>['<=def','<='],  
+  '>='=>['>=def','>='],  
+  '<'=>['<def','<'],  
+  '>'=>['>def','>'],  
+  '->'=>['->def','->'],  
+  '<=>'=>['<=>def','<=>'],  
+  ','=>[',def',','],  
+  '['=>['[def','['],  
+  ']'=>[']def',']'],  
+  '>>'=>['>>def','>>'],  
+  '<<'=>['<<def','<<'],         
+  '"abc"'=>['"abc" def','string','"abc"'],  
+  "'abc'"=>["'abc' def",'string','"abc"'],  
+  '"abc\"def"'=>['"abc\"def" def','string','abc "def'],  
+  "'abc\'def'"=>["'abc\'def' def",'string',"abc 'def"],  
+  "."=>[".def",'.'],  
+  '2025-02-25'=>['2025-02-25 def','date','2025-02-25'],  
+  '2025-02-25 02:02:22'=>['2025-02-25 02:02:22 def','datetime','2025-02-25 02:02:22'],  
+  '02:02:22'=>['02:02:22 def','time','02:02:22'],
+  'null'=>["null def","null"],
 )];
 
 test('Test move pointer', function() 
