e38a7ce8ab19bedc218476bb72f9d952b87f97d7
Klaus Dimde <klaus@dimde.info>
2025-02-25 21:25:19 +0100

    Fixed lexer

--- src/Query/Lexer.php
+++ src/Query/Lexer.php
@@ -2,11 +2,47 @@
 
 namespace Sunhill\Query;
 
+use Sunhill\Basic\Base;
+use Sunhill\Query\Exceptions\InvalidTokenException;
+
 class Lexer extends Base
 {
   protected $parse_string = '';
 
   protected $pointer = 0;
+  
+  const MAX_SYMBOL_LEN = 4;
+  
+  const SYMBOLS = [
+      'null'=>'NULL',
+      '<=>'=>'<=>',
+      '=='=>'=',
+      '<='=>'<=',
+      '>='=>'>=',
+      '->'=>'->',
+      '>>'=>'>>',
+      '<<'=>'<<',
+      '!='=>'!=',
+      '<>'=>'<>',
+      '('=>'(',
+      ')'=>')',
+      '='=>'=',
+      '-'=>'-',
+      '+'=>'+',
+      '*'=>'*',
+      '/'=>'/',
+      '<'=>'<',
+      '>'=>'>',
+      '['=>'[',
+      ']'=>']',
+      ','=>',',
+      '%'=>'%',
+      '&'=>'&',
+      '|'=>'|',
+      '^'=>'^',
+      '~'=>'~',
+      '.'=>'.'
+  ];
   
   public function __construct(string $parse_string)
   {
@@ -16,14 +52,87 @@ class Lexer extends Base
 
   private function getNextCharacters(int $count): string
   {
-      return substr($this->parse_string, $pointer, $count);
+      return substr($this->parse_string, $this->pointer, $count);
   }
 
   private function getNextIdentifier(): ?string
   {
-      if (preg_match('/^([a-zA-Z_][_[:alnum:]]*)',substr($this->parse_str,$this->pointer),$matches) {
+      if (preg_match('/^([a-zA-Z_][_[:alnum:]]*)/',substr($this->parse_string,$this->pointer),$matches)) {
           return $matches[1];
-      }    
+      } 
+      return null;
+  }
+  
+  private function getSymbol(): ?\stdClass
+  {
+      for ($i=Lexer::MAX_SYMBOL_LEN;$i>0;$i--) {
+          $next = strtolower($this->getNextCharacters($i));
+          if (array_key_exists($next,Lexer::SYMBOLS)) {
+              $this->pointer += $i;
+              return makeStdClass(['type'=>Lexer::SYMBOLS[$next]]);
+          }
+      }
+      return null;
+  }
+  
+  private function getIdentifier(): ?\stdClass
+  {
+      if ($identifier = $this->getNextIdentifier()) {
+          $this->pointer += strlen($identifier);
+          return makeStdClass(['type'=>'ident','value'=>$identifier]);
+      }
+      return null;
+  }
+  
+  private function getStringConstant(): ?\stdClass
+  {
+      if ($this->parse_string[$this->pointer] == '"') {
+          $ending_symbol = '"';
+      } else if ($this->parse_string[$this->pointer] == "'") {
+          $ending_symbol = "'";
+      } else {
+          return null;
+      }
+      $this->pointer++;
+      $result = '';
+      while ($this->pointer < strlen($this->parse_string) && ($this->parse_string[$this->pointer]) !== $ending_symbol) {
+          if ($this->parse_string[$this->pointer] == '\\') {
+              $this->pointer++;
+              if ($this->pointer >= strlen($this->parse_string)) {
+                  throw new InvalidTokenException("The string is not closed");
+              }
+          }
+          $result .= $this->parse_string[$this->pointer++];
+      }
+      if ($this->pointer >= strlen($this->parse_string)) {
+          throw new InvalidTokenException("The string is not closed");
+      }
+      return makeStdClass(['type'=>'const','field_type'=>'str','value'=>$result]);
+  }
+  
+  function getDateConstant()
+  {
+      if (preg_match("/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01]) (0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])/",substr($this->parse_string,$this->pointer),$matches)) {
+          return makeStdClass(['type'=>'const','field_type'=>'datetime','value'=>$matches[0]]);
+      }
+      if (preg_match("/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01])/",substr($this->parse_string,$this->pointer),$matches)) {
+          return makeStdClass(['type'=>'const','field_type'=>'date','value'=>$matches[0]]);
+      }
+      if (preg_match("/^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])/",substr($this->parse_string,$this->pointer),$matches)) {
+          return makeStdClass(['type'=>'const','field_type'=>'time','value'=>$matches[0]]);
+      }
+      return null;
+  }
+  
+  function getNumericConstant()
+  {
+      if (preg_match('/^-?\d+(\.\d+)?/',substr($this->parse_string,$this->pointer),$matches)) {
+          if (strpos($matches[0],'.') !== false) {
+              return makeStdClass(['type'=>'const','field_type'=>'float','value'=>$matches[0]]);
+          } else {
+              return makeStdClass(['type'=>'const','field_type'=>'int','value'=>$matches[0]]);              
+          }              
+      }
   }
   
   public function getNextToken(): ?\stdClass
@@ -31,18 +140,25 @@ class Lexer extends Base
     if ($this->pointer >= strlen($this->parse_string)) {
       return null; // EOL
     }
-    $next = $this->getNextCharacters(3);
-    if (in_array($next,['<=>'])) {
-      return makeStdClass(['type'=>$next]);
+    while ($this->pointer < strlen($this->parse_string) && ($this->parse_string[$this->pointer] == ' ')) {
+        $this->pointer++;
     }
-    $next = $this->getNextCharacters(2);
-    if (in_array($next,['==','<=','>=','->','>>','<<'])) {
-      return makeStdClass(['type'=>$next]);      
+    if ($symbol = $this->getSymbol()) {
+        return $symbol;
     }
-    $next = $this->getNextCharacters(1);
-    if (in_array($next,['(',')','=','-','+','*','/','<','>','[',']',','])) {
-      return makeStdClass(['type'=>$next]);      
-    }    
+    if ($identifier = $this->getIdentifier()) {
+        return $identifier;
+    }
+    if ($string = $this->getStringConstant()) {
+        return $string;
+    }
+    if ($string = $this->getDateConstant()) {
+        return $string;
+    }
+    if ($string = $this->getNumericConstant()) {
+        return $string;
+    }
+    throw new InvalidTokenException("Can't process token: ".substr($this->parse_string,$this->pointer));
   }
  
 }
--- tests/Unit/Query/LexerTest.php
+++ tests/Unit/Query/LexerTest.php
@@ -1,27 +1,35 @@
 <?php
 
+use Sunhill\Tests\SunhillTestCase;
+use Sunhill\Query\Lexer;
+use Sunhill\Query\Exceptions\InvalidTokenException;
+
 uses(SunhillTestCase::class);
 
 test('Test EOL', function() 
 {
     $test = new Lexer('');
-    expect($test->getNextToken())->tBe(null);
+    expect($test->getNextToken())->toBe(null);
 });
 
-test('Test special chars',function($input, $token, $value = null)
+test('Test special chars',function($input, $token, $value = null, $type = null)
 {
     $test = new Lexer($input);
-    $token = $test->getNextToken();
-    expect($token->type)->toBe($token);
+    $result = $test->getNextToken();
+    expect($result->type)->toBe($token);
     if ($value) {
-        expect($token->vaue)->toBe($value);
+        expect($result->value)->toBe($value);
     }            
+    if ($type) {
+        expect($result->field_type)->toBe($type);
+    }
 })->with([
-  'identifier'=>['abc def','ident','abc'],  
+    'null'=>["null def","NULL"],
+    'identifier'=>['abc def','ident','abc'],  
   'identifier with numbers and underscore'=>['abc_d3 def','ident','abc_d3'],  
   'identifier starting with underscore'=>['_abc def','ident','_abc'],  
-  'integer'=>['123 def','int','123'],  
-  'float'=>['1.23 def','float','1.23'],  
+  'integer'=>['123 def','const','123','int'],  
+  'float'=>['1.23 def','const','1.23','float'],  
   '('=>['(def','('],  
   ')'=>[')def',')'],  
   '='=>['=def','='],  
@@ -34,7 +42,7 @@ test('Test special chars',function($input, $token, $value = null)
   '|'=>['|def','|'],  
   '~'=>['~def','~'],  
   '^'=>['^def','^'],  
-  '=='=>['==def','=='],  
+  '=='=>['==def','='],  
   '<='=>['<=def','<='],  
   '>='=>['>=def','>='],  
   '<'=>['<def','<'],  
@@ -46,28 +54,33 @@ test('Test special chars',function($input, $token, $value = null)
   ']'=>[']def',']'],  
   '>>'=>['>>def','>>'],  
   '<<'=>['<<def','<<'],         
-  '"abc"'=>['"abc" def','string','"abc"'],  
-  "'abc'"=>["'abc' def",'string','"abc"'],  
-  '"abc\"def"'=>['"abc\"def" def','string','abc "def'],  
-  "'abc\'def'"=>["'abc\'def' def",'string',"abc 'def"],  
+  '"abc"'=>['"abc" def','const','abc','str'],  
+  "'abc'"=>["'abc' def",'const','abc','str'],  
+  '"abc\"def"'=>['"abc\"def" def','const','abc"def','str'],  
+  "'abc\'def'"=>["'abc\'def' def",'const',"abc'def",'str'],  
   "."=>[".def",'.'],  
-  '2025-02-25'=>['2025-02-25 def','date','2025-02-25'],  
-  '2025-02-25 02:02:22'=>['2025-02-25 02:02:22 def','datetime','2025-02-25 02:02:22'],  
-  '02:02:22'=>['02:02:22 def','time','02:02:22'],
-  'null'=>["null def","null"],
-)];
+  '2025-02-25'=>['2025-02-25 def','const','2025-02-25','date'],  
+  '2025-02-25 02:02:22'=>['2025-02-25 02:02:22 def','const','2025-02-25 02:02:22','datetime'],  
+  '02:02:22'=>['02:02:22 def','const','02:02:22','time'],
+]);
 
 test('Test move pointer', function() 
 {
     $test = new Lexer('abc def');
     $test->getNextToken();
-    expect($test->getNextToken())->toBe('def');
+    expect($test->getNextToken()->value)->toBe('def');
 });
 
 test('Test move pointer and skip multiple whitespaces', function() 
 {
     $test = new Lexer('abc    def');
     $test->getNextToken();
-    expect($test->getNextToken())->toBe('def');
+    expect($test->getNextToken()->value)->toBe('def');
 });
+
+it('fails when string is not closed', function()
+{
+    $test = new Lexer('"abc def');
+    $test->getNextToken();
+})->throws(InvalidTokenException::class);
 
