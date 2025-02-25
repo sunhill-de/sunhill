0a8d6cb80d27b776061f8f032b8c87804e39e24a
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-25 15:10:59 +0100

    scan symbols

--- src/Query/Lexer.php
+++ src/Query/Lexer.php
@@ -10,13 +10,39 @@ class Lexer extends Base
   
   public function __construct(string $parse_string)
   {
+      $this->parse_string = $parse_string;
+      $this->pointer = 0;
   }
 
+  private function getNextCharacters(int $count): string
+  {
+      return substr($this->parse_string, $pointer, $count);
+  }
+
+  private function getNextIdentifier(): ?string
+  {
+      if (preg_match('/^([a-zA-Z_][_[:alnum:]]*)',substr($this->parse_str,$this->pointer),$matches) {
+          return $matches[1];
+      }    
+  }
+  
   public function getNextToken(): ?\stdClass
   {
     if ($this->pointer >= strlen($this->parse_string)) {
       return null; // EOL
     }
+    $next = $this->getNextCharacters(3);
+    if (in_array($next,['<=>'])) {
+      return makeStdClass(['type'=>$next]);
+    }
+    $next = $this->getNextCharacters(2);
+    if (in_array($next,['==','<=','>=','->','>>','<<'])) {
+      return makeStdClass(['type'=>$next]);      
+    }
+    $next = $this->getNextCharacters(1);
+    if (in_array($next,['(',')','=','-','+','*','/','<','>','[',']',','])) {
+      return makeStdClass(['type'=>$next]);      
+    }    
   }
-  
+ 
 }
