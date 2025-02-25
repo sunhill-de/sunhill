67e19f0494d9ce790f9699b6936f09f2cfe3c827
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-25 10:21:09 +0100

    Implemented some tests

--- tests/Unit/Query/LexerTest.php
+++ tests/Unit/Query/LexerTest.php
@@ -8,8 +8,32 @@ test('Test EOL', function()
     expect($test->getNextToken())->tBe(null);
 });
 
-test('Test identifier (only characters)', function() 
+test('Test special chars',function($input, $token)
+{
+    $test = new Lexer($input);
+    expect($test->getNextToken())->toBe($token);
+})->with([
+  'identifier'=>['abc def','abc'],  
+  'identifier with numbers and underscore'=>['abc_d3 def','abc_d3'],  
+  'identifier starting with underscore'=>['_abc def','_abc'],  
+  'integer'=>['123 def','123'],  
+  'float'=>['1.23 def','1.23'],  
+  '('=>['(def','('],  
+  '('=>[')def',')'],  
+  '='=>['=def','='],  
+)];
+
+test('Test move pointer', function() 
 {
     $test = new Lexer('abc def');
-    expect($test->getNextToken())->tBe('abc');
+    $test->getNextToken();
+    expect($test->getNextToken())->toBe('def');
 });
+
+test('Test move pointer and skip multiple whitespaces', function() 
+{
+    $test = new Lexer('abc    def');
+    $test->getNextToken();
+    expect($test->getNextToken())->toBe('def');
+});
+
