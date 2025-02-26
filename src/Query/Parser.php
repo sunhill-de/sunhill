34e5421989dd32a2cf0afcc6c54b11e3fad1dfb6
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-26 10:12:25 +0100

    Updated grammar

--- src/Query/Parser.php
+++ src/Query/Parser.php
@@ -5,11 +5,14 @@ namespace Sunhill\Query;
 class Parser extends QueryHandler
 {
     const GRAMMAR = [
-        'EXPRESSION'=>['EXPRESSION|+|SUM','EXPRESSION|-|SUM','SUM','€'],
+        'EXPRESSION'=>[['EXPRESSION','||','XOREXPRESSION'],['XOREXPRESSION'],['€']],
+        'XOREXPRESSION'=>[['XOREXPRESSION','xor','ANDEXPRESSION'],['ANDEXPRESSION'],['€']],
+    [['EXPRESSION','+','SUM'],['EXPRESSION','-','SUM'],['SUM'],['€']],
         'SUM'=>['SUM|*|FAKTOR','SUM|/|FAKTOR','SUM|%|FAKTOR','SUM|mod|FAKTOR','SUM|div|FAKTOR','FAKTOR','€'],
         'FAKTOR'=>['(|EXPRESSION|]','const','field','field|as|ident','FUNCT','€'],
         'FUNCT'=>['ident|(|LIST|)'],
         'LIST'=>['EXPRESSION','EXPRESSION|,|EXPRESSION','€'],
-        'ORDER'=['field','field|asc','field|desc'], 
+        'ORDER'=>['field','field|asc','field|desc'],
+        'ASSIGN'=>['field','=','EXPRESSION'], // For set statements
     ];
 }  
