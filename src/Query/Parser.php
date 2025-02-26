a7228f31b550d9d07e0f4aa558cd262aa078c5e6
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2025-02-26 11:14:39 +0100

    more Grammar

--- src/Query/Parser.php
+++ src/Query/Parser.php
@@ -5,9 +5,28 @@ namespace Sunhill\Query;
 class Parser extends QueryHandler
 {
     const GRAMMAR = [
-        'EXPRESSION'=>[['EXPRESSION','||','XOREXPRESSION'],['XOREXPRESSION'],['€']],
-        'XOREXPRESSION'=>[['XOREXPRESSION','xor','ANDEXPRESSION'],['ANDEXPRESSION'],['€']],
-    [['EXPRESSION','+','SUM'],['EXPRESSION','-','SUM'],['SUM'],['€']],
+        'EXPRESSION'=>[['EXPRESSION','||','XOREXPRESSION'],['XOREXPRESSION']],
+        'XOREXPRESSION'=>[['XOREXPRESSION','xor','ANDEXPRESSION'],['ANDEXPRESSION']],
+        'ANDEXPRESSION'=>[['ANDEXPRESSION','and','COMPEXPRESSION'],['COMPEXPRESSION]],
+        'COMPEXPRESSION'=>[
+            ['COMPEXPRESSION','=','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','!=','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','>=','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','<=','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','>','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','<','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','<=>','BETWEENEXPRESSION'],
+            ['COMPEXPRESSION','IS','BETWEENEXPRESSION'],
+            ['BETWEENEXPRESSION'],
+        ],
+        'BETWEENEXPRESSION'=>[['BETWEEN','BETWEENLIMIT','AND','BETWEENLIMIT'],['LIKEEXPRESSION']],
+        'VALUEEXPRESSION'=>[['const'],['field']],
+        'LIKEEXPRESSION'=>[['VALUEFIELD','like','VALUEFIELD'],['VALUEFIELD','regexp','VALUEFIELD'],['VALUEFIELD','in','VALUEFIELD'],['BITWISEOR']],
+        'BITWISEOR'=>[['VALUEFIELD','|','VALUEFIELD'],['BITWISEAND']],
+        'BITWISEAND'=>[['VALUEFIELD','&','VALUEFIELD'],['SHIFTEXPRESSION']],
+        'SHIFTEXPRESSION'=>[['VALUEFIELD','>>','VALUEFIELD'],['VALUEFIELD','<<','VALUEFIELD'],['ADDEXPRESSION']],
+    [
+    ['EXPRESSION','+','SUM'],['EXPRESSION','-','SUM'],['SUM'],['€']],
         'SUM'=>['SUM|*|FAKTOR','SUM|/|FAKTOR','SUM|%|FAKTOR','SUM|mod|FAKTOR','SUM|div|FAKTOR','FAKTOR','€'],
         'FAKTOR'=>['(|EXPRESSION|]','const','field','field|as|ident','FUNCT','€'],
         'FUNCT'=>['ident|(|LIST|)'],
