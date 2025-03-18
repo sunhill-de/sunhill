dac82477042dc38da42ffcb065fd7ac72654600c
Klaus Dimde <klaus@dimde.info>
2025-03-18 17:23:45 +0100

    Implemented analyzer specific exceptions

---
+++ src/Parser/Exceptions/AnalyzerException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file AnalyzerException.php
+ * Provides the AnalyzerException
+ * Lang en
+ * Reviewstatus: 2025-03-14
+ * Localization: none
+ * Documentation: complete
+ */
+
+namespace Sunhill\Parser\Exceptions;
+
+/**
+ * Basic exception that is raised inside an analyzer
+ * @author klaus
+ *
+ */
+class AnalyzerException extends ParsingSubsystemException {}
---
+++ src/Parser/Exceptions/IdentifierNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file IdentifierNotFoundException.php
+ * Provides the IdentifierNotFoundException
+ * Lang en
+ * Reviewstatus: 2025-03-14
+ * Localization: none
+ * Documentation: complete
+ */
+
+namespace Sunhill\Parser\Exceptions;
+
+/**
+ * Basic exception that is raised inside an analyzer
+ * @author klaus
+ *
+ */
+class IdentifierNotFoundException extends AnalyzerException {}
