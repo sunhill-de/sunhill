27dca550ca66a5ed1428e2851d9ce9bb6b53843b
Klaus Dimde <klaus@dimde.info>
2025-03-22 19:44:49 +0100

    Implemented user exception

---
+++ src/Exceptions/SunhillUserException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file SunhillUserException.php
+ * Provides root for all sunhill user exceptions. A user exception is an exception that may be displayed
+ * to the user because he or she did something wrong (no programming error)
+ */
+
+namespace Sunhill\Exceptions;
+
+
+/**
+ * Root for sunhill user exceptions
+ * @author lokal
+ *
+ */
+class SunhillUserException extends \Exception {}
