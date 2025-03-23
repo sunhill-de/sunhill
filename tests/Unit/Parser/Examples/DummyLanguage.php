4d7224993eeca514d7677cbfc234674eb05c8eb0
Klaus Dimde <klaus@dimde.info>
2025-03-23 08:13:54 +0100

    Completed DummyLanguage

--- tests/Unit/Parser/Examples/DummyLanguage.php
+++ tests/Unit/Parser/Examples/DummyLanguage.php
@@ -30,12 +30,14 @@ class DummyLanguage extends LanguageDescriptor
             ->setType('binary')
             ->setPrecedence(5)
             ->addTypes('boolean','boolean','boolean')
+            ->addTypes('boolean','pseudoboolean','boolean')
             ->addTypes('pseudoboolean','boolean','boolean')
             ->addTypes('pseudoboolean','pseudoboolean','boolean');            
         $this->addOperator('xor')
             ->setType('binary')
             ->setPrecedence(10)
             ->addTypes('boolean','boolean','boolean')
+            ->addTypes('boolean','pseudoboolean','boolean')
             ->addTypes('pseudoboolean','boolean','boolean')
             ->addTypes('pseudoboolean','pseudoboolean','boolean');
         $this->addOperator('&&')
@@ -44,23 +46,99 @@ class DummyLanguage extends LanguageDescriptor
             ->addTypes('boolean','boolean','boolean')
             ->addTypes('pseudoboolean','boolean','boolean')
             ->addTypes('pseudoboolean','pseudoboolean','boolean');            
-        $this->addOperator('=')->setType('binary')->setPrecedence(20);
-        $this->addOperator('<=')->setType('binary')->setPrecedence(20);
-        $this->addOperator('>=')->setType('binary')->setPrecedence(20);
-        $this->addOperator('<>')->setType('binary')->setPrecedence(20);
-        $this->addOperator('|')->setType('binary')->setPrecedence(25);
-        $this->addOperator('&')->setType('binary')->setPrecedence(30);      
-        $this->addOperator('>>')->setType('binary')->setPrecedence(35);
-        $this->addOperator('<<')->setType('binary')->setPrecedence(35);        
-        $this->addOperator('+')->setType('binary')->setPrecedence(35);
-        $this->addOperator('-')->setType('binary')->setPrecedence(35);        
-        $this->addOperator('/')->setType('binary')->setPrecedence(40);
-        $this->addOperator('*')->setType('binary')->setPrecedence(40);
-        $this->addOperator('div')->setType('binary')->setPrecedence(40);
-        $this->addOperator('%')->setType('binary')->setPrecedence(40);        
-        $this->addOperator('^')->setType('binary')->setPrecedence(45);        
-        $this->addOperator('!')->setType('unary')->setPrecedence(50);
-        $this->addOperator('->')->setType('binary')->setPrecedence(55);
+        $this->addOperator('=')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('pseudoboolean','pseudoboolean','boolean')
+            ->addTypes('array','array','boolean');
+        $this->addOperator('<=')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('numeric','numeric','boolean')
+            ->addTypes('string','string','boolean');
+        $this->addOperator('>=')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('numeric','numeric','boolean')
+            ->addTypes('string','string','boolean');
+        $this->addOperator('<')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('numeric','numeric','boolean')
+            ->addTypes('string','string','boolean');
+        $this->addOperator('>')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('numeric','numeric','boolean')
+            ->addTypes('string','string','boolean');
+        $this->addOperator('<>')
+            ->setType('binary')
+            ->setPrecedence(20)
+            ->addTypes('numeric','numeric','boolean')
+            ->addTypes('string','string','boolean');            
+        $this->addOperator('|')
+            ->setType('binary')
+            ->setPrecedence(25)
+            ->addTypes('integer','integer','integer');
+        $this->addOperator('&')
+            ->setType('binary')
+            ->setPrecedence(27)
+            ->addTypes('integer','integer','integer');        
+        $this->addOperator('>>')
+            ->setType('binary')
+            ->setPrecedence(30)
+            ->addTypes('integer','integer','integer');        
+        $this->addOperator('<<')
+            ->setType('binary')
+            ->setPrecedence(30)
+            ->addTypes('integer','integer','integer');        
+        $this->addOperator('+')
+            ->setType('binary')
+            ->setPrecedence(35)
+            ->addTypes('integer','integer','integer')
+            ->addTypes('integer','float','float')
+            ->addTypes('float','float','float')
+            ->addTypes('float','integer','float')
+            ->addTypes('string','string','string');            
+        $this->addOperator('-')
+            ->setType('binary')
+            ->setPrecedence(35)
+            ->addTypes('integer','integer','integer')
+            ->addTypes('integer','float','float')
+            ->addTypes('float','float','float')
+            ->addTypes('float','integer','float');            
+        $this->addOperator('/')
+            ->setType('binary')
+            ->setPrecedence(40)
+            ->addTypes('integer','float','float');            
+        $this->addOperator('*')
+            ->setType('binary')
+            ->setPrecedence(40)
+            ->addTypes('integer','integer','integer')
+            ->addTypes('integer','float','float')
+            ->addTypes('float','float','float')
+            ->addTypes('float','integer','float');        
+        $this->addOperator('div')
+            ->setType('binary')
+            ->setPrecedence(40)
+            ->addTypes('integer','integer','integer');
+        $this->addOperator('%')
+            ->setType('binary')
+            ->setPrecedence(40)        
+            ->addTypes('integer','integer','integer');
+        $this->addOperator('^')
+            ->setType('binary')
+            ->setPrecedence(45)        
+            ->addTypes('integer','integer','integer');
+        $this->addOperator('!')
+            ->setType('unary')
+            ->setPrecedence(50)
+            ->addTypes('boolean','boolean')
+            ->addTypes('psudoboolean','boolean');
+        $this->addOperator('->')
+            ->setType('binary')
+            ->setPrecedence(55)
+            ->addTypes('identifier','identifier');
     }
     
 }
