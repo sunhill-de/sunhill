684ebe0bdc9d3cd220c6b9aadbe5838b6d15d1b1
Klaus Dimde <klaus@dimde.info>
2025-03-25 14:35:28 +0100

    Implemented MethodSignature

--- src/Query/Helpers/MethodSignature.php
+++ src/Query/Helpers/MethodSignature.php
@@ -3,23 +3,19 @@
 namespace Sunhill\Query\Helpers;
 
 use Sunhill\Basic\Base;
+use Illuminate\Support\Collection;
+use Sunhill\Properties\RecordProperty;
+use Sunhill\Query\Query;
 
 class MethodSignature extends Base
 {
-    protected string $name = '';
-
     protected array $parameter = [];
 
     protected $action;
     
-    public function __construct(string $name)
+    public function addParameter(string $parameter_signature): static
     {
-       $this->name = $name;
-    }
-
-    public function addParamter(string $parameter_signature): static
-    {
-        $this->parameters[] = $parameter_signature;
+        $this->parameter[] = $parameter_signature;
 
         return $this;
     }  
@@ -45,14 +41,14 @@ class MethodSignature extends Base
             return false;
         }
         for ($i=0; $i<count($this->parameter); $i++) {
-            if (!$this->signature_matches($test_parameters[$i], $this->parameter[$i])) {
+            if (!$this->signature_matches(static::getSignature($test_parameters[$i]), $this->parameter[$i])) {
                 return false;
             }    
         }
         return true;
     }    
 
-    static privat function getSignatureOfArrayElements(array|Traversable $param): string
+    static private function getSignatureOfArrayElements(array|\Traversable $param): string
     {
         $current_signature = '';
         foreach ($param as $element) {
@@ -60,7 +56,7 @@ class MethodSignature extends Base
             if (empty($current_signature)) {
                 $current_signature = $test;
                 continue;
-            } else if (($current_signature == $test) || (($test == 'integer') && ($current_signature == 'float')) {
+            } else if (($current_signature == $test) || (($test == 'integer') && ($current_signature == 'float'))) {
                 continue;
             } else if (($test == 'float') && ($current_signature == 'integer')) {
                 $current_signature = 'float';
@@ -68,7 +64,9 @@ class MethodSignature extends Base
                 $current_signature = 'mixed';
             }
         } 
+        return $current_signature;
     }    
+    
     static public function getSignature($param): string
     {
         if (is_string($param)) {
@@ -80,8 +78,23 @@ class MethodSignature extends Base
         if (is_float($param)) {
             return 'float';
         }  
-        if (is_array($param)) {
+        if (is_bool($param)) {
+            return 'boolean';
+        }
+        if (is_array($param) || is_a($param, Collection::class)) {
             return 'array of '.static::getSignatureOfArrayElements($param);
-        }    
+        }
+        if (is_callable($param)) {
+            return 'callback';
+        }
+        if (is_a($param, RecordProperty::class)) {
+            return 'record';
+        }
+        if (is_a($param, Query::class)) {
+            return 'subquery';
+        }
+        if (is_object($param)) {
+            return 'object';
+        }
     }    
 }  
--- tests/Unit/Query/MethodSignatureTest.php
+++ tests/Unit/Query/MethodSignatureTest.php
@@ -1,6 +1,9 @@
 <?php
 
-use Sunhill/Tests/SunhillTestCase;
+use Sunhill\Tests\SunhillTestCase;
+use Sunhill\Query\Helpers\MethodSignature;
+use Sunhill\Tests\TestSupport\Objects\Dummy;
+use Sunhill\Query\Query;
 
 uses(SunhillTestCase::class);
 
@@ -8,13 +11,60 @@ test('getSignature', function($parameter, $expect)
 {
   expect(MethodSignature::getSignature($parameter))->toBe($expect);
 })->with([
-  [10,'integer'],
-  [10.23,'float'],
-  ["abc",'string'],
-  [[1,2,3],'array of integer'],
-  [[1.2,2.3,3.4],'array of float'],
-  [[1,2.3,3],'array of float'],
-  [["a","b","c"],'array of string'],
-  [[1,1.2,"c"],'array of mixed'],         
+    [10,'integer'],
+    [10.23,'float'],
+    ["abc",'string'],
+    [true, 'boolean'],
+    [[1,2,3],'array of integer'],
+    [[1.2,2.3,3.4],'array of float'],
+    [[1,2.3,3],'array of float'],
+    [["a","b","c"],'array of string'],
+    [[1,1.2,"c"],'array of mixed'],
+    [collect([1,2,3]),'array of integer'],
+    [collect([1.2,2.3,3.4]),'array of float'],
+    [collect([1,2.3,3]),'array of float'],
+    [collect(["a","b","c"]),'array of string'],
+    [collect([1,1.2,"c"]),'array of mixed'],
+    [function() { return 12; },'callback'],
+    [new Dummy(), 'record'],
+    [new Query(), 'subquery'],
+    [new \stdClass(), 'object']
 ]);         
        
+test('matches', function($callback, $params, $expect)
+{
+    $test = new MethodSignature();
+    $callback($test);
+    
+    expect($test->matches($params))->toBe($expect);
+})->with([
+    [function($signature)
+    {
+        $signature->addParameter('integer');
+    },[10],true
+    ],
+    [function($signature)
+    {
+        $signature->addParameter('integer');
+        $signature->addParameter('string');
+    },[10,'ABC'],true
+    ],
+    [function($signature)
+    {
+        $signature->addParameter('integer');
+        $signature->addParameter('string');
+    },['ABC',10],false
+    ],
+    [function($signature)
+    {
+        $signature->addParameter('integer');
+        $signature->addParameter('string');
+    },[10],false
+    ],
+    [function($signature)
+    {
+        $signature->addParameter('integer');
+        $signature->addParameter('string');
+    },[10,'ABC',20],false
+    ],
+    ]);
