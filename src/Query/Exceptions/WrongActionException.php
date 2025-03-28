9d8c9bbc515abd08f871a9c916db2d9e114c602f
Klaus Dimde <klaus@dimde.info>
2025-03-28 13:16:11 +0100

    Implemented WrongActionException

---
+++ src/Query/Exceptions/WrongActionException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file WrongActionException.php
+ * Provides the WrongActionException
+ * Lang en
+ * Reviewstatus: 2024-10-07
+ * Localization: none
+ * Documentation: complete
+ */
+
+namespace Sunhill\Query\Exceptions;
+
+/**
+ * Is raised when a wrong action is passed for a method signature
+ * @author klaus
+ *
+ */
+class WrongActionException extends QueryException {}
