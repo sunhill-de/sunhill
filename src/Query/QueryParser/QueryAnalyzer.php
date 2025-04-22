0097ee00821db2c9a47c77163985f447a7d1563a
Klaus Dimde <klaus@dimde.info>
2025-04-22 20:50:40 +0200

    Prepared QueryAnalyzer

---
+++ src/Query/QueryParser/QueryAnalyzer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @file QueryAnalyzer.php
+ * The parser that analyzes query strings 
+ * Lang en
+ * Reviewstatus: 2025-04-25
+ * Creation date: 2025-04-25
+ * Localization: complete
+ * Documentation: complete
+ * Tests: 
+ * Coverage:
+ */
+
+namespace Sunhill\Query\QueryParser;
+
+use Sunhill\Parser\Analyzer;
+
+class QueryAnalyzer extends Analyzer
+{
+        
+    public function __construct()
+    {
+        $this->loadLanguageDescriptor(new QueryParserLanguage());    
+    }
+        
+}
