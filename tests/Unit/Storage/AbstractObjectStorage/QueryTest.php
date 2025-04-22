cfd8a079f614b2fa0f42d923d63072a59b2e5e26
Klaus Dimde <klaus@dimde.info>
2025-04-22 20:47:56 +0200

    Implemented query test

---
+++ tests/Unit/Storage/AbstractObjectStorage/QueryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use Sunhill\Tests\SunhillTestCase;
+use Sunhill\Query\QueryParser\QueryNode;
+use Sunhill\Tests\Unit\Storage\AbstractObjectStorage\DummyAbstractObjectStorage;
+use Sunhill\Facades\Properties;
+use Sunhill\Parser\Nodes\BinaryNode;
+use Sunhill\Parser\Nodes\IdentifierNode;
+use Sunhill\Parser\Nodes\IntegerNode;
+
+uses(SunhillTestCase::class);
+
+function getStorageForQuery()
+{
+    Properties::shouldReceive('loadAttribute')->with(1,1)->andReturn(['attribute1'=>'value1']);
+    Properties::shouldReceive('loadAttribute')->with(2,1)->andReturn(['attribute2'=>'value2']);
+    Properties::shouldReceive('loadAttribute')->with(2,2)->andReturn(['attribute2'=>'value2']);
+    Properties::shouldReceive('loadAttribute')->with(3,2)->andReturn(['attribute3'=>'value3']);
+    Properties::shouldReceive('loadAttribute')->with(4,3)->andReturn(['attribute4'=>'value4']);
+
+    $test = new DummyAbstractObjectStorage();
+    $test::$DataPool = $test::$Data;  
+    return $test;
+}
+
+
+test('Execute query for select without anything else', function()
+{
+    $test = getStorageForQuery();
+    $query = new QueryNode();
+    $query->verb('select');
+    
+    $result = $test->executeQuery($query);
+    
+    expect(count($result))->toBe(4);
+});
+
+test('Execute query for select with a where statement', function()
+{
+    $test = getStorageForQuery();
+    $query = new QueryNode();
+    $query->verb('select');
+    $where = new BinaryNode('<');
+    $where->left(new IdentifierNode('child_int'));
+    $where->right(new IntegerNode(333));
+    $query->setWhere($where);
+    
+    $result = $test->executeQuery($query);
+    
+    expect(count($result))->toBe(2);
+});
