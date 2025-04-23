0ea74f2aa709f0e1255b350aef055a3d105ffd9c
Klaus Dimde <klaus@dimde.info>
2025-04-23 17:47:27 +0200

    Prepared ArrayObjectStorage

---
+++ src/Storage/ArrayObjectStorage/ArrayObjectStorage.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @file ArrayObjectStorage.php
+ * This is the implementation of an AbstractObjectStorage that stores the values in arrays
+ *
+ * @author Klaus Dimde
+ * Lang en
+ * Reviewstatus: 2025-04-23
+ * Creation date: 2025-04-23
+ * Localization: none
+ * Documentation: unknown
+ * Tests: /tests/Unit/Storage/ArrayObjectStorage/*
+ * Coverage:
+ */
+
+namespace Sunhill\Storage\ArrayObjectStorage;
+
+use Sunhill\Storage\AbstractObjectStorage;
+use Sunhill\Query\QueryParser\QueryNode;
+
+class ArrayObjectStorage extends AbstractObjectStorage
+{
+    
+    /**
+     * Updates the storage with the subid. It uses $key to identiy the record(s) and sets the givenvalues
+     *
+     * @param unknown $key
+     * @param unknown $values
+     */
+    protected function updateStorageSubid(string $subid, int $key, array $values, string $key_field = 'id')
+    {
+        
+    }
+    
+    /**
+     * Deletes all references to key from the given
+     *
+     * @param unknown $key
+     */
+    protected function deleteStorageSubid(string $subid, int $key, string $key_field = 'id')
+    {
+        
+    }
+    
+    /**
+     * Inserts into the given storage subid the given values. If value is a array of arrays then insert every entry as a separate record
+     *
+     * @param string $subid
+     * @param array $values
+     */
+    protected function insertStorageSubid(string $subid, array $values)
+    {
+        
+    }
+    
+    protected function insertObjects(array $values): int
+    {
+        
+    }
+    
+    /**
+     * Loads from the given storage subif the values with the given key
+     *
+     * @param string $subid
+     * @param int $key
+     * @return array
+     */
+    protected function loadStorageSubid(string $subid, int $key, string $key_field = 'id'): array|\stdClass
+    {
+        
+    }
+    
+    protected function doExecuteQuery(QueryNode $node)
+    {
+        
+    }
+    
+}
