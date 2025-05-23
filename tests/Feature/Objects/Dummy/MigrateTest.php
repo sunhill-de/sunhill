2c22c51c648e01fcfc37f0bbb0961cfb741289d3
Klaus Dimde <klaus@dimde.info>
2025-05-23 20:29:01 +0200

    Implemented more feature migration tests

--- tests/Feature/Objects/Dummy/MigrateTest.php
+++ tests/Feature/Objects/Dummy/MigrateTest.php
@@ -3,14 +3,84 @@
 use Sunhill\Tests\SunhillDatabaseTestCase;
 use Sunhill\Facades\Properties;
 use Sunhill\Tests\TestSupport\Objects\Dummy;
+use Illuminate\Support\Facades\Schema;
 
 uses(SunhillDatabaseTestCase::class);
 
-test('migrate Dummy', function()
+test('migrate Dummy with nothing to do', function()
 {
     Properties::registerProperty(Dummy::class);
     Dummy::migrate();
     
     $this->assertDatabaseHasTable('dummies');
-});
+})->group('migrate');
+
+test('migrate Dummy fresh', function()
+{
+    Schema::dropIfExists('dummies');
+    Properties::registerProperty(Dummy::class);
+    Dummy::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+})->group('migrate');
+
+test('migrate Dummy with a dropped field', function()
+{
+    Schema::dropIfExists('dummies');
+    Schema::create('dummies', function($table)
+    {
+       $table->integer('id');
+       $table->integer('dummyint');
+       $table->string('dropfield');
+    });
+
+    Properties::registerProperty(Dummy::class);
+    Dummy::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseTableHasColumn('dummies','dummyint');
+    $this->assertDatabaseTableHasNotColumn('dummies','dropped');
+    $this->assertDatabaseTableHasColumn('dummies','id');
+    $this->assertDatabaseTableColumnIsType('dummies', 'dummyint', 'integer');
+    $this->assertDatabaseTableColumnIsType('dummies', 'id', 'integer');
+    
+})->group('migrate');
+
+test('migrate Dummy with an added field', function()
+{
+    Schema::dropIfExists('dummies');
+    Schema::create('dummies', function($table)
+    {
+        $table->integer('id');
+    });
+    
+    Properties::registerProperty(Dummy::class);
+    Dummy::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseTableHasColumn('dummies','dummyint');
+    $this->assertDatabaseTableHasColumn('dummies','id');
+    $this->assertDatabaseTableColumnIsType('dummies', 'dummyint', 'integer');
+    $this->assertDatabaseTableColumnIsType('dummies', 'id', 'integer');
+    
+})->group('migrate');
+
+test('Migrate with a field type changed', function()
+{
+    Schema::dropIfExists('dummies');
+    Schema::create('dummies', function($table) 
+    {
+        $table->integer('id')->primary();
+        $table->string('dummyint');
+    });
+        
+    Properties::registerProperty(Dummy::class);
+    Dummy::migrate();
+        
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseTableHasColumn('dummies','dummyint');
+    $this->assertDatabaseTableHasColumn('dummies','id');
+    $this->assertDatabaseTableColumnIsType('dummies', 'dummyint', 'integer');
+    $this->assertDatabaseTableColumnIsType('dummies', 'id', 'integer');
+})->group('migrate');
 
--- tests/Feature/Objects/DummyChild/MigrateTest.php
+++ tests/Feature/Objects/DummyChild/MigrateTest.php
@@ -4,13 +4,54 @@ use Sunhill\Tests\SunhillDatabaseTestCase;
 use Sunhill\Facades\Properties;
 use Sunhill\Tests\TestSupport\Objects\Dummy;
 use Sunhill\Tests\TestSupport\Objects\DummyChild;
+use Illuminate\Support\Facades\Schema;
 
 uses(SunhillDatabaseTestCase::class);
 
-test('migrate DummyChild', function()
+test('migrate DummyChild with nothing to do', function()
 {
     Properties::registerProperty(Dummy::class);
     Properties::registerProperty(DummyChild::class);
+    DummyChild::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseHasTable('dummychildren');
+});
+
+test('migrate DummyChild with both tables missing', function()
+{
+    Properties::registerProperty(Dummy::class);
+    Properties::registerProperty(DummyChild::class);
+
+    Schema::dropIfExists('dummies');
+    Schema::dropIfExists('dummychildren');
+    
+    DummyChild::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseHasTable('dummychildren');
+});
+
+test('migrate DummyChild with dummychildren table missing', function()
+{
+    Properties::registerProperty(Dummy::class);
+    Properties::registerProperty(DummyChild::class);
+    
+    Schema::dropIfExists('dummychildren');
+    
+    DummyChild::migrate();
+    
+    $this->assertDatabaseHasTable('dummies');
+    $this->assertDatabaseHasTable('dummychildren');
+});
+
+test('migrate DummyChild with dummies table missing', function()
+{
+    Properties::registerProperty(Dummy::class);
+    Properties::registerProperty(DummyChild::class);
+    
+    Schema::dropIfExists('dummies');
+    
     DummyChild::migrate();
     
     $this->assertDatabaseHasTable('dummies');
