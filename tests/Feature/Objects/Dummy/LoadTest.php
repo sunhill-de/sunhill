68c260ddeb6027fa623581f1b3eb7b08587504a1
Klaus Dimde <klaus@dimde.info>
2025-05-25 18:46:14 +0200

    More load feature tests

--- tests/Feature/Objects/Dummy/LoadTest.php
+++ tests/Feature/Objects/Dummy/LoadTest.php
@@ -2,6 +2,9 @@
 
 use Sunhill\Tests\SunhillDatabaseTestCase;
 use Sunhill\Tests\TestSupport\Objects\Dummy;
+use Sunhill\Storage\Exceptions\InvalidIDException;
+use Sunhill\Storage\Exceptions\IDNotFoundException;
+use Sunhill\Properties\Exceptions\InvalidPropertyException;
 
 uses(SunhillDatabaseTestCase::class);
 
@@ -11,4 +14,25 @@ test('Load a dummy from database', function()
     $test = new Dummy();    
     $test->load(1);
     expect($test->dummyint)->toBe(123);
-});
+})->group('load');
+
+it('fails when id is invalid', function()
+{
+    Dummy::prepareDatabase($this);
+    $test = new Dummy();
+    $test->load('abc');    
+})->group('load')->throws(InvalidIDException::class);
+
+it('fails when id does not exist', function()
+{
+    Dummy::prepareDatabase($this);
+    $test = new Dummy();
+    $test->load(999);
+})->group('load')->throws(IDNotFoundException::class);
+
+it('fails when id does not match expected class', function()
+{
+    Dummy::prepareDatabase($this);
+    $test = new Dummy();
+    $test->load(9);
+})->group('load')->throws(InvalidPropertyException::class);
