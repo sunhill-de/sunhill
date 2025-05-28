f91de888f5a1de3159905e0d92ec15ab2ab48c00
Klaus Dimde <klaus@dimde.info>
2025-05-28 23:11:53 +0200

    New exceptions for tag handling

---
+++ src/Tags/Exceptions/TagNameAmbiguousException.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * @file TagNameAmbiguousException.php
+ * Provides the TagNameAmbiguousException
+ * Lang en
+ * Reviewstatus: 2025-05-28
+ * Createstatus: 2025-05-28
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
+namespace Sunhill\Tags\Exceptions;
+
+/**
+ * is raised when the name was not not unique
+ * @author lokal
+ *
+ */
+class TagNameAmbiguousException extends TagException
+{
+}
---
+++ src/Tags/Exceptions/TagNameNotFoundException.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * @file TagNameNotFoundException.php
+ * Provides the TagNameNotFoundException
+ * Lang en
+ * Reviewstatus: 2021-12-09
+ * Localization: none
+ * Documentation: complete
+ * Tests: none
+ * Coverage: unknown
+ */
+
+namespace Sunhill\Tags\Exceptions;
+
+/**
+ * is raised when the name was not found
+ * @author lokal
+ *
+ */
+class TagNameNotFoundException extends TagException
+{
+}
