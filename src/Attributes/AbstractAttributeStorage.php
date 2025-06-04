ea4297f3f45091eb131e3cdc88b8b89a7dcdb0ed
Klaus Dimde <klaus@dimde.info>
2025-06-04 22:13:54 +0200

    Implemented abstract searchAttribute

--- src/Attributes/AbstractAttributeStorage.php
+++ src/Attributes/AbstractAttributeStorage.php
@@ -19,4 +19,6 @@ abstract class AbstractAttributeStorage extends Base
 {
     
     abstract public function loadAttribute(int $container_id, int $attribute_id);
+    
+    abstract public function searchAttribute(string $name): ?\stdClass;
 }
