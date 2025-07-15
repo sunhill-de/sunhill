9b518758678ce93d5d23ffa1fd25d39b163939f7
Klaus Dimde <klaus@dimde.info>
2025-07-15 15:04:26 +0200

    Implemented LanguageDescriptorException

---
+++ src/Parser/Exceptions/LanguageDescriptorException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file LanguageDescriptorException.php
+ * Provides the ParserException
+ * Lang en
+ * Reviewstatus: 2025-07-15
+ * Localization: none
+ * Documentation: complete
+ */
+
+namespace Sunhill\Parser\Exceptions;
+
+/**
+ * Basic exception that is raised when an exception is raised in the LanguageDescriptor
+ * @author klaus
+ *
+ */
+class LanguageDescriptorException extends ParsingSubsystemException {}
