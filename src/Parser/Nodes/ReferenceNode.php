3afbb4960505e3d0b94a8ae6cabf3400d8bdd2a6
Klaus Dimde <klaus@dimde.info>
2025-07-15 13:56:42 +0200

    Implemented missing functionality of ReferenceNode

--- src/Parser/Nodes/ReferenceNode.php
+++ src/Parser/Nodes/ReferenceNode.php
@@ -3,26 +3,30 @@
  * @file ReferenceNode.php
  * A node that represents an identifier pointing to a subfield
  * Lang en
- * Reviewstatus: 2025-06-29
+ * Reviewstatus: 2025-07-15
  * Create date: 2025-06-29
  * Localization: complete
  * Documentation: complete
- * Tests: Unit/Parser/NodeTest.php
+ * Tests: Unit/Parser/Nodes/ReferenceNodeTest.php
  * Coverage Unit: 
  */
 
 namespace Sunhill\Parser\Nodes;
 
 use Sunhill\Parser\Traits\UnknownDatatype;
+use Sunhill\Query\Exceptions\InvalidStatementException;
 
 class ReferenceNode extends TerminalNode
 {
   
     use UnknownDatatype;
     
-    public function __construct($value)
+    public function __construct($value, ?Node $reference = null)
     {
         parent::__construct('reference',$value);
+        if ($reference) {
+            $this->reference($reference);
+        }
     }
     
     /**
@@ -33,13 +37,53 @@ class ReferenceNode extends TerminalNode
         return $this->getValue();
     }
     
+    /**
+     * Getter and setter for reference
+     * 
+     * @param Node $reference
+     * @return \Sunhill\Parser\Nodes\ReferenceNode|NULL|mixed
+     */
     public function reference(?Node $reference = null)
     {
         return $this->handleReplacingChild('reference', $reference);
     }    
     
+    /**
+     * Checks if this reference is valid. First checks if there is a reference at all, if yes call validate of the reference
+     * 
+     * {@inheritDoc}
+     * @see \Sunhill\Parser\Nodes\Node::validate()
+     */
     public function validate()
     {
+        if (!$this->reference()) {
+            throw new InvalidStatementException("No reference is set.");
+        }
+        $this->reference()->validate();
     }
     
+    /**
+     * Converte the reference to a string
+     * 
+     * {@inheritDoc}
+     * @see \Sunhill\Parser\Nodes\Node::toString()
+     */
+    public function toString(): string
+    {
+        return $this->getName().'->'.$this->reference()->toString();        
+    }
+    
+    /**
+     * Returns the datatype of the reference
+     * 
+     * {@inheritDoc}
+     * @see \Sunhill\Parser\Nodes\Node::getDatatype()
+     */
+    public function getDatatype(): ?string
+    {
+        if ($this->reference()) {
+            return $this->reference()->getDatatype();
+        }
+        return null;
+    }
 }
---
+++ tests/Unit/Parser/Nodes/ReferenceNodeTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @file ReferenceNodeTest.php
+ * tests: /src/Parser/Nodes/ReferenceNode.php
+ * free of dependent units: yes
+ */
+
+use Sunhill\Tests\SunhillTestCase;
+use Sunhill\Parser\Nodes\FunctionNode;
+use Sunhill\Parser\LanguageDescriptor\FunctionDescriptor;
+use Sunhill\Parser\Nodes\Node;
+use Sunhill\Parser\Nodes\ArrayNode;
+use Sunhill\Parser\Exceptions\AnalyzerException;
+use Sunhill\Parser\Nodes\ReferenceNode;
+use Sunhill\Query\Exceptions\InvalidStatementException;
+
+uses(SunhillTestCase::class);
+
+test('getDatatype() of reference with a type set', function()
+{
+    $reference = \Mockery::mock(Node::class);
+    $reference->shouldReceive('getDatatype')->once()->andReturn('integer');
+    $test = new ReferenceNode('test', $reference);
+    expect($test->getDatatype())->toBe('integer');
+});
+
+test('getDatatype() of function without a type set', function()
+{
+    $reference = \Mockery::mock(Node::class);
+    $reference->shouldReceive('getDatatype')->once()->andReturn(null);
+    $test = new ReferenceNode('test', $reference);
+    expect($test->getDatatype())->toBe(null);
+});
+
+test('toString() works', function()
+{
+    $reference = \Mockery::mock(Node::class);
+    $reference->shouldReceive('toString')->once()->andReturn('sub');
+    $test = new ReferenceNode('main', $reference);
+    expect($test->toString())->toBe('main->sub');
+    
+});
+
+test('getName() works', function()
+{
+    $reference = \Mockery::mock(Node::class);
+    $test = new ReferenceNode('main', $reference);
+    expect($test->getName())->toBe('main');    
+});
+
+test('validate() works', function()
+{
+    $reference = \Mockery::mock(Node::class);
+    $reference->shouldReceive('validate')->once();
+    $test = new ReferenceNode('main', $reference);
+    $test->validate();
+});
+
+test('validate() fails when no reference is set', function()
+{
+    $test = new ReferenceNode('main');
+    $test->validate();
+})->throws(InvalidStatementException::class);
+
+
