a20d825eb9327e0a4a5cb567d7fefca63ddfaffe
Klaus Dimde <klaus@dimde.info>
2025-07-22 06:23:20 +0200

    Fixed append routine

--- tests/Feature/Objects/BigParent/BigParentTest.php
+++ tests/Feature/Objects/BigParent/BigParentTest.php
@@ -2,18 +2,27 @@
 
 use Sunhill\Tests\SunhillDatabaseTestCase;
 use Sunhill\Tests\TestSupport\Objects\BigParent;
+use Sunhill\Tests\Database\Seeds\ObjectsSeeder;
+use Sunhill\Tests\Database\Seeds\DummiesSeeder;
+use Sunhill\Tests\Database\Seeds\TagsSeeder;
 
 uses(SunhillDatabaseTestCase::class);
 
 test('Migrate and seed', function()
 {
+    $this->seed([
+        ObjectsSeeder::class,
+        DummiesSeeder::class,
+        TagsSeeder::class,
+    ]);
     BigParent::migrate();
     foreach (BigParent::$TEST_DATA as $entry) {
         $object = new BigParent();
+        $object->create();
         foreach ($entry as $key => $value) {
             if (is_array($value)) {
                 foreach ($value as $array_key => $array_value) {                   
-                    if (is_int($array_value)) {
+                    if (is_int($array_key)) {
                         $object->$key[] = $array_value;
                     } else {
                         $object->$key[$array_key] = $array_value;
