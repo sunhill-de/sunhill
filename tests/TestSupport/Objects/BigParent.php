f2c05addc05bfcf31ebb0dce1548745119830532
Klaus Dimde <klaus@dimde.info>
2025-07-22 06:22:46 +0200

    Fixed field properties

--- tests/TestSupport/Objects/BigParent.php
+++ tests/TestSupport/Objects/BigParent.php
@@ -30,15 +30,15 @@ class BigParent extends ORMObject
         $builder->addProperty(TypeDateTime::class,'p_datetime');
         $builder->addProperty(TypeBoolean::class,'p_bool')->default(true);
         $builder->referRecord(Dummy::class, 'p_dummy_ref');
-        $builder->referRecord(BigParent::class,'p_self_ref')->setAllowedProperty(BigParent::class);
-        $builder->referRecord(Dummy::class,'p_mult_ref')->setAllowedProperty(ChildObject::class);
+        $builder->referRecord(BigParent::class,'p_self_ref')->setAllowedProperty(BigParent::class)->nullable();
+        $builder->referRecord(Dummy::class,'p_mult_ref')->setAllowedProperty([Dummy::class,ChildObject::class]);
         $builder->array('p_s_array')->setAllowedElementType(TypeVarchar::class);
         $builder->array('p_i_array')->setAllowedElementType(TypeInteger::class);
         $builder->array('p_f_array')->setAllowedElementType(TypeFloat::class);
         $builder->array('p_s_map')->setIndexType('string')->setAllowedElementType(TypeVarchar::class);
         $builder->array('p_i_map')->setIndexType('string')->setAllowedElementType(TypeInteger::class);
         $builder->array('p_f_map')->setIndexType('string')->setAllowedElementType(TypeFloat::class);
-        $builder->arrayOfReferences('p_r_array')->setAllowedElementType(DummyChild::class);
+        //$builder->arrayOfReferences('p_r_array')->setAllowedElementType(DummyChild::class);
     }
     
     protected static function setupInfos()
@@ -68,7 +68,27 @@ class BigParent extends ORMObject
             'p_s_map'=>['keyA'=>'ValueA','keyB'=>'ValueB'],
             'p_i_map'=>['No1'=>498,'No2'=>296,'LastNo'=>199],
             'p_f_map'=>['a'=>3.89,'b'=>2.99,'c'=>12.986],
-            'p_r_array'=>[1,2,3]
+          //  'p_r_array'=>[1,2,3]
+        ],
+        [
+            'p_int'=>294,
+            'p_string'=>'Muse',
+            'p_float'=>1.92,
+            'p_date'=>'2022-06-21',
+            'p_time'=>'06:40:00',
+            'p_datetime'=>'2022-11-24 17:23:11',
+            'p_bool'=>false,
+            'p_dummy_ref'=>2,
+            'p_self_ref'=>null,
+            'p_mult_ref'=>1,
+            'p_s_array'=>['pascal','fortran','c++'],
+            'p_i_array'=>[198,432,832],
+            'p_f_array'=>[3.89,2.99,12.986],
+            'p_s_map'=>['keyA'=>'NewA','keyB'=>'NewB','keyC'=>'NewC'],
+            'p_i_map'=>['No1'=>111,'No2'=>298],
+            'p_f_map'=>['a'=>192.22,'c'=>9.445],
+          //  'p_r_array'=>[2]
         ]
+        
     ];
 }
