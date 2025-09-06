892472370bf0ce110feb68241d74e3d891672579
Klaus Dimde <klaus@dimde.info>
2025-09-06 17:53:59 +0200

    Reworked FunctionDescriptor

--- src/Parser/LanguageDescriptor/FunctionDescriptor.php
+++ src/Parser/LanguageDescriptor/FunctionDescriptor.php
@@ -16,6 +16,7 @@
 namespace Sunhill\Parser\LanguageDescriptor;
 
 use Sunhill\Basic\Base;
+use Sunhill\Parser\Exceptions\LanguageDescriptorException;
 
 class FunctionDescriptor extends Base
 {
@@ -36,57 +37,188 @@ class FunctionDescriptor extends Base
      */
     protected array $parameters = [];
 
-    public function __construct(string $descriptor)
+    /**
+     * Helper function that returns the last added parameter
+     * 
+     * @return array|NULL
+     */
+    private function stackTop(?int $position = null): ?array
     {
-        preg_match('/(.*)\:([_a-zA-Z0-9]*)\((.*)\)\:(.*)/', $descriptor, $matches);
-        $this->context = $matches[1];
-        $this->name = $matches[2];
-        $this->parameters = empty($matches[3]) ? [] : explode(',', $matches[3]);
-        $this->return_type = $matches[4];
+        if (!count($this->parameters)) {
+            return null;
+        }
+        return ($this->parameters[is_null($position)?count($this->parameters)-1:$position]);
+    }
+    
+    /**
+     * Creates a new function descriptor and sets its name
+     * 
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
     }
 
+    /**
+     * Getter for the name
+     * 
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * Setter for return type
+     * 
+     * @param string $type
+     * @return static
+     */
+    public function setReturnType(string $type): static
+    {
+        $this->return_type = $type;
+        
+        return $this;
+    }
+    
+    /**
+     * Getter for return type
+     * 
+     * @return string
+     */
     public function getReturnType(): string
     {
         return $this->return_type;
     }
 
+    /**
+     * Setter for context. The context is an addional information to tell the parse, in which context a
+     * function could be used.
+     * 
+     * @param string $context
+     * @return static
+     */
+    public function setContext(string $context): static
+    {
+        $this->context = $context;
+        
+        return $this;
+    }
+    
+    /**
+     * Getter for context.
+     * 
+     * @return string
+     */
+    public function getContext(): string
+    {
+        return $this->context;
+    }
+    
+    /**
+     * Adds an mandatory parameter that has to be passed to this function
+     * 
+     * @param string $type The type of this parameter
+     * @return static
+     */
+    public function addMandatoryParameter(string $type): static
+    {
+        if ((count($this->parameters)) && ($this->stackTop()['param_class'] == 'optional')) {
+            throw new LanguageDescriptorException("In a function descriptor an mandatory parameter must't be after an optional");
+        }
+        if ((count($this->parameters)) && ($this->stackTop()['param_class'] == 'ellipsis')) {
+            throw new LanguageDescriptorException("In a function descriptor an mandatory parameter must't be after an ellipsis");
+        }
+        
+        $this->parameters[] = ['type'=>$type, 'param_class'=>'mandatory'];
+        
+        return $this;
+    }
+    
+    /**
+     * Adds an optional parameter that can but doesn't have  to be passed to this function
+     *
+     * @param string $type The type of this parameter
+     * @return static
+     */
+    public function addOptionalParameter(string $type): static
+    {
+        if ((count($this->parameters)) && ($this->stackTop()['param_class'] == 'ellipsis')) {
+            throw new LanguageDescriptorException("In a function descriptor an optional parameter must't be after an ellipsis");
+        }
+        
+        $this->parameters[] = ['type'=>$type, 'param_class'=>'optional'];        
+        
+        return $this;
+    }
+
+    /**
+     * Adds an ellipsis. An ellipis is a variable count of parameters of the same type. 
+     * 
+     * @param string $type
+     * @return static
+     */
+    public function addEllipsis(string $type): static
+    {
+        $this->parameters[] = ['type'=>$type, 'param_class'=>'ellipsis'];        
+        
+        return $this;
+    }
+    
+    /**
+     * Returns the count of parameters
+     * 
+     * @return int
+     */
     public function getParameterCount(): int
     {
         return count($this->parameters);
     }
 
-    public function getContext(): string
-    {
-        return $this->context;
-    }
-
+    /**
+     * Used internally as a stack pointer
+     * 
+     * @var integer
+     */
     private int $stack_pointer = 0;
-
+    
+    /**
+     * Resets the "stack" for this function
+     */
     public function reset()
     {
         $this->stack_pointer = 0;
     }
-
+    
+    /**
+     * "pops" the next possible parameter and returns its type (or null if no parameter is left)
+     * 
+     * @return string|NULL
+     */
     public function pop(): ?string
     {
+        if (!count($this->parameters)) {
+            return null;
+        }
         if ($this->stack_pointer >= count($this->parameters)) {
             return null;
         }
-        $result = $this->parameters[$this->stack_pointer++];
-        if (substr($result, 0, 3) == '...') {
-            $this->stack_pointer--;
-
-            return '?'.substr($result, 3);
+        $result = $this->stackTop($this->stack_pointer)['type'];
+        if ($this->stackTop($this->stack_pointer)['param_class'] !== 'ellipsis') {
+            $this->stack_pointer++;
         }
-        if ($result[0] !== '?') {
-            return '!'.$result;
-        }
-
         return $result;
+    }
+
+    /**
+     * Returns true when all mandatory parameters where popped
+     * 
+     * @return bool
+     */
+    public function mandatoryFinished(): bool
+    {
+        return (!count($this->parameters) || ($this->stackTop($this->stack_pointer))['parameters_type'] !== 'mandatory');
     }
 }
--- tests/Unit/Parser/LanguageDescriptor/FunctionDescriptorTest.php
+++ tests/Unit/Parser/LanguageDescriptor/FunctionDescriptorTest.php
@@ -8,61 +8,118 @@
 
 use Sunhill\Parser\LanguageDescriptor\FunctionDescriptor;
 use Sunhill\Tests\SunhillSimpleTestCase;
+use Sunhill\Parser\Exceptions\LanguageDescriptorException;
 
 uses(SunhillSimpleTestCase::class);
 
-test('FunctionDescriptor with no parameters', function () {
-    $test = new FunctionDescriptor('any:testfunct():integer');
-    expect($test->getName())->toBe('testfunct');
-    expect($test->getReturnType())->toBe('integer');
-    expect($test->getParameterCount())->toBe(0);
-    expect($test->getContext())->toBe('any');
+test('Constructor sets name', function()
+{
+   $test = new FunctionDescriptor('testfunc');
+   expect($test->getName())->toBe('testfunc');
+});
+
+test('setReturnType() and getReturnType()', function()
+{
+   $test = new FunctionDescriptor('testfunc');
+   $test->setReturnType('integer');
+   expect($test->getReturnType())->toBe('integer');
+});
+
+test('setContext() and getContext()', function()
+{
+    $test = new FunctionDescriptor('testfunc');
+    $test->setContext('some_context');
+    expect($test->getContext())->toBe('some_context');
+});
+
+test('addMandatoryParameter()', function()
+{
+   $test = new FunctionDescriptor('testfunct');
+   $test->addMandatoryParameter('integer');
+   expect($test->getParameterCount())->toBe(1);
+});
+
+test('addOptionalParameter()', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addOptionalParameter('integer');
+    expect($test->getParameterCount())->toBe(1);
+});
+
+test('addEllipsis()', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addEllipsis('integer');
+    expect($test->getParameterCount())->toBe(1);
+});
+
+test('Function stack with no parameters', function()
+{
+    $test = new FunctionDescriptor('testfunct');
     $test->reset();
     expect($test->pop())->toBe(null);
 });
 
-test('FunctionDescriptor with one mandatory parameter', function () {
-    $test = new FunctionDescriptor('any:testfunct(integer):integer');
+test('Function stack with one mandatory', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addMandatoryParameter('integer');
     $test->reset();
-    expect($test->pop())->toBe('!integer');
+    expect($test->pop())->toBe('integer');
     expect($test->pop())->toBe(null);
 });
 
-test('FunctionDescriptor with two mandatory parameters', function () {
-    $test = new FunctionDescriptor('any:testfunct(integer,string):integer');
+test('Function stack with two mandatory', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addMandatoryParameter('integer');
+    $test->addMandatoryParameter('string');
     $test->reset();
-    expect($test->pop())->toBe('!integer');
-    expect($test->pop())->toBe('!string');
+    expect($test->pop())->toBe('integer');
+    expect($test->pop())->toBe('string');
     expect($test->pop())->toBe(null);
 });
 
-test('FunctionDescriptor with one mandatory parameter and one optional', function () {
-    $test = new FunctionDescriptor('any:testfunct(integer,?string):integer');
+test('Function stack with one mandatory and one optional', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addMandatoryParameter('integer');
+    $test->addOptionalParameter('string');
     $test->reset();
-    expect($test->pop())->toBe('!integer');
-    expect($test->pop())->toBe('?string');
+    expect($test->pop())->toBe('integer');
+    expect($test->pop())->toBe('string');
     expect($test->pop())->toBe(null);
 });
 
-test('FunctionDescriptor with two optional parameters', function () {
-    $test = new FunctionDescriptor('any:testfunct(?integer,?string):integer');
+test('Function stack with one mandatory and ellipsis', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addMandatoryParameter('integer');
+    $test->addEllipsis('string');
     $test->reset();
-    expect($test->pop())->toBe('?integer');
-    expect($test->pop())->toBe('?string');
-    expect($test->pop())->toBe(null);
+    expect($test->pop())->toBe('integer');
+    expect($test->pop())->toBe('string');
+    expect($test->pop())->toBe('string');
 });
 
-test('FunctionDescriptor with ellipsis parameters', function () {
-    $test = new FunctionDescriptor('any:testfunct(...integer):integer');
-    $test->reset();
-    expect($test->pop())->toBe('?integer');
-    expect($test->pop())->toBe('?integer');
-});
+it('Fails when adding a mandatory after an optional', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addOptionalParameter('string');
+    $test->addMandatoryParameter('integer');    
+})->throws(LanguageDescriptorException::class);
 
-test('FunctionDescriptor with one mandatory annd ellipsis parameters', function () {
-    $test = new FunctionDescriptor('any:testfunct(integer,...integer):integer');
-    $test->reset();
-    expect($test->pop())->toBe('!integer');
-    expect($test->pop())->toBe('?integer');
-    expect($test->pop())->toBe('?integer');
-});
+it('Fails when adding an optional after an ellipsis', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addEllipsis('integer');
+    $test->addOptionalParameter('string');
+})->throws(LanguageDescriptorException::class);
+
+it('Fails when adding an mandatory after an ellipsis', function()
+{
+    $test = new FunctionDescriptor('testfunct');
+    $test->addEllipsis('integer');
+    $test->addOptionalParameter('string');
+})->throws(LanguageDescriptorException::class);
+
