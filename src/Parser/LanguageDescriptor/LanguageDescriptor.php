76c1ccc7b3c64774a121efd33e9c578d09499f52
Klaus Dimde <klaus@dimde.info>
2025-09-06 07:16:46 +0200

    Restructured LanguageDescriptor

--- src/Parser/LanguageDescriptor/LanguageDescriptor.php
+++ src/Parser/LanguageDescriptor/LanguageDescriptor.php
@@ -5,7 +5,7 @@
  * A helper class that makes it easier to define a language that can be interpreted by the
  * lexer, parser and analyzer
  * Lang en
- * Reviewstatus: 2025-07-15
+ * Reviewstatus: 2025-09-04
  * Create date: 2025-03-19
  * Localization: complete
  * Documentation: complete
@@ -26,6 +26,20 @@ class LanguageDescriptor extends Base
      * @var array
      */
     protected $terminals = [];
+    
+    /**
+     * A collection of the rules that apply to this parser
+     * 
+     * @var array
+     */
+    protected $parser_rules = [];
+    
+    /**
+     * All the parser rules that are accepted as a valid expression if only this is left on the stack
+     * 
+     * @var array
+     */
+    protected $accepted_finals = [];
     
     /**
      * Adds a terminal to this language. A terminal is a piece that the lexer returns on request. 
@@ -49,5 +63,54 @@ class LanguageDescriptor extends Base
     public function getTerminals(): array
     {
         return $this->terminals;
+    }
+    
+    /**
+     * Creates a new parser rule, adds it to the desciptor and returns it for further manipulations
+     * 
+     * @param string $left_side
+     * @param string|array $right_side
+     * @return ParserRule
+     */
+    public function addRule(string $left_side, string|array $right_side): ParserRule
+    {
+        $rule = new ParserRule($left_side, $right_side);
+        $this->parser_rules[] = $rule; 
+        
+        return $rule;
+    }
+    
+    /**
+     * Returns the defined parser rules (used by parser and analyzer)
+     * 
+     * @return array
+     */
+    public function getParserRules(): array
+    {
+        return $this->parser_rules;
+    }
+    
+    /**
+     * Adds one or more non-terminals that are accepted as a last element on the stack
+     * 
+     * @param array|string $final
+     */
+    public function addAcceptedFinal(array|string $final)
+    {
+        if (is_array($final)) {
+            $this->accepted_finals = array_merge($this->accepted_finals,$final);
+        } else {
+            $this->accepted_finals[] = $final;
+        }
+    }
+    
+    /**
+     * Returns the accepted finals array
+     * 
+     * @return array
+     */
+    public function getAcceptedFinals(): array
+    {
+        return $this->accepted_finals;
     }
 }
--- tests/TestSupport/Parser/DummyLanguage.php
+++ tests/TestSupport/Parser/DummyLanguage.php
@@ -28,7 +28,96 @@ class DummyLanguage extends LanguageDescriptor
         $this->addTerminal('*');
         $this->addTerminal('/');
         $this->addTerminal('->');
-/*        
+        
+        $this->addRule('EXPRESSION', ['SUM'])
+            ->setASTCallback('twoSideOperator')
+            ->setPriority(5);
+        $this->addRule('SUM', ['SUM','+','PRODUCT'])
+            ->setASTCallback('twoSideOperator')
+            ->setPriority(15)
+            ->setTypes([
+                ['integer','integer','integer'],
+                ['integer','float','float'],
+                ['float','integer','float'],
+                ['float','float','float'],
+                ['string','string','string']
+            ]);
+        $this->addRule('SUM', ['SUM','-','PRODUCT'])
+            ->setASTCallback('twoSideOperator')
+            ->setPriority(15)
+            ->setTypes([
+                ['integer','integer','integer'],
+                ['integer','float','float'],
+                ['float','integer','float'],
+                ['float','float','float'],
+            ]);
+        $this->addRule('SUM', ['PRODUCT'])
+            ->setASTCallback('passThrough')
+            ->setPriority(10);
+        $this->addRule('PRODUCT', ['PRODUCT','*', 'FACTOR'])            
+            ->setASTCallback('twoSideOperator')
+            ->setPriority(25)
+            ->setTypes([
+                ['integer','integer','integer'],
+                ['integer','float','float'],
+                ['float','integer','float'],
+                ['float','float','float'],
+            ]);
+        $this->addRule('PRODUCT', ['PRODUCT','/', 'FACTOR'])
+            ->setASTCallback('twoSideOperator')
+            ->setPriority(25)
+            ->setTypes([
+                ['integer','integer','integer'],
+                ['integer','float','float'],
+                ['float','integer','float'],
+                ['float','float','float'],
+            ]);
+        $this->addRule('PRODUCT', ['FACTOR'])
+            ->setASTCallback('passThrough')
+            ->setPriority(20);
+        $this->addRule('FACTOR',['-','UNARYMINUS'])
+            ->setASTCallback('oneSideOperator')
+            ->setPriority(30)
+            ->setTypes([
+                ['integer','integer'],
+                ['float','float']
+            ]);
+        $this->addRule('FACTOR',['(','SUM',')'])
+             ->setASTCallback('bracket')
+             ->setPriority(30);
+        $this->addRule('FACTOR','CONST')
+             ->setASTCallback('passThrough')
+             ->setPriority(40);
+        $this->addRule('FACTOR','ident')
+             ->setASTCallback('passThrough')
+             ->setPriority(40);
+        $this->addRule('FACTOR','FUNCTION')
+             ->setASTCallback('passThrough')
+             ->setPriority(40);
+        $this->addRule('CONST', 'integer')
+             ->setASTCallback('passThrough')
+             ->setPriority(50);
+        $this->addRule('CONST', 'float')
+             ->setASTCallback('passThrough')
+             ->setPriority(50);
+        $this->addRule('CONST', 'string')
+             ->setASTCallback('passThrough')
+             ->setPriority(50);
+        $this->addRule('CONST', 'boolean')
+             ->setASTCallback('passThrough')
+             ->setPriority(50);
+        $this->addRule('FUNCTION',['ident','(',')'])
+             ->setASTCallback('functionHandler')
+             ->setPriority(50);
+        $this->addRule('FUNCTION',['ident','(','EXPRESSION',')'])
+             ->setASTCallback('functionHandler')
+             ->setPriority(50);
+
+        $this->addFunction('sin')
+             ->setReturnType('float')
+             ->setArguments([])
+        $this->addAcceptedSymbol('EXPRESSION');
+        /*        
         $this->addBinaryOperator('||')
             ->setType('binary')
             ->setPrecedence(5)
