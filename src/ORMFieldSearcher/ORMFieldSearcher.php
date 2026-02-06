decd5702253fe6efcd724d0ed85c58c5a391f44f
Klaus Dimde <klaus@dimde.info>
2026-02-06 21:18:06 +0100

    Added the ORMFieldSearcher class

---
+++ src/ORMFieldSearcher/ORMFieldSearcher.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file ORMFieldSearcher/ORMFieldSearcher.php
+ * Provides the ORMFieldSearcher class.
+ * Last reviewed: 2026-02-06
+ * Created: 2026-02-06
+ * Author: Klaus
+ */
+
+namespace Sunhill\ORMFieldSearcher;
+
+/**
+ * Class ORMFieldSearcher
+ * 
+ * Defines a class that searches a field in a descriptor array and return the field descriptor.
+ * 
+ * @package ORM
+ * @author klaus
+ * @since 1.0.0
+ */
+class ORMFieldSearcher
+{
+    
+}
